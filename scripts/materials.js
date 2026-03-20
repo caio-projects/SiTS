d9f2b2f178f17eea30c2508ab021e2320d9b2001
Pedro Caio Lima <pedrocaioslima@gmail.com>
2026-03-20 00:02:24 -0300

    go

---
+++ scripts/materials.js
@@ -0,0 +1,7 @@
+// Optimization: Find minimum rate among materials
+const materials = [
+    { name: "Superalloy X", params: { A: 1e-10, n: 4.5, Q: 250000 } },
+    { name: "Titanium Alloy", params: { A: 1e-8, n: 5.0, Q: 200000 } }
+];
+
+//
